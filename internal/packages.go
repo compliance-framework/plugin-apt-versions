e31a70395891112c8968a71f0027895096b0b57c
Steven Trotter <stevetrot@gmail.com>
2025-03-25 12:17:52 +0000

    Reimplemented the previous sed logic into Go and added lots of testing to check it's behaving as it used to do before (with a couple of minor fixes too)

--- internal/packages.go
+++ internal/packages.go
@@ -34,21 +34,22 @@ func GetInstalledPackages(logger hclog.Logger) (map[string]any, []*proto.Step, e
 	}
 
 	// Parse the output into a map
-	packages := getPackages(dpkgStdout.String())
+	packages := getPackages(logger, dpkgStdout.String())
 
 	return packages, steps, nil
 }
 
-func getPackages(output string) map[string]any {
+func getPackages(logger hclog.Logger, packageData string) map[string]any {
 	packages := make(map[string]any)
 
-	for _, line := range strings.Split(output, "\n") {
+	for _, line := range strings.Split(packageData, "\n") {
 		if len(line) == 0 {
 			continue
 		}
 
 		parts := strings.Split(line, " ")
 		if len(parts) != 2 {
+			logger.Warn(fmt.Sprintf("unexpected number of parts in package line, cannot process: %s", line))
 			continue
 		}
 
@@ -62,50 +63,51 @@ func getPackages(output string) map[string]any {
 }
 
 func getVersion(version string) string {
-	// // If the version contains a colon, we'll split the string and return the version from the second part
-	// if colonIndex := strings.Index(version, ":"); colonIndex != -1 {
-	// 	return version[colonIndex+1:]
-	// }
+	// If the version contains a colon, we'll split the string and return the version from the second part
+	if colonIndex := strings.Index(version, ":"); colonIndex != -1 {
+		version = version[colonIndex+1:]
+	}
 
-	// // If we have any of the characters, '-', '+', '~', we'll split the string and return the version from the first part
-	// if dashIndex := strings.IndexAny(version, "-+~"); dashIndex != -1 {
-	// 	version = version[:dashIndex]
-	// }
+	// If we have any of the characters, '-', '+', '~', we'll split the string and return the version from the first part
+	if dashIndex := strings.IndexAny(version, "-+~"); dashIndex != -1 {
+		version = version[:dashIndex]
+	}
 
-	// // Split the version into parts
-	// parts := strings.Split(version, ".")
+	// Split the version into parts
+	parts := strings.Split(version, ".")
 
-	// // Check each part it just a number, if it's not we'll split the part and only keep the number
-	// for i, part := range parts {
-	// 	// If the part contains a string after a number we skip from the number onwards
-	// 	for j, char := range part {
-	// 		if char >= '0' && char <= '9' {
-	// 			continue
-	// 		}
-	// 		parts[i] = part[:j]
-	// 		break
-	// 	}
-	// }
+	// Check each part is just a number, if it's not we'll split the part and only keep the number
+	for i, part := range parts {
+		// If the part contains a string after a number we skip from the number onwards
+		for j, char := range part {
+			if char >= '0' && char <= '9' {
+				continue
+			}
+			parts[i] = part[:j]
+			break
+		}
+	}
 
-	// // Check each part for leading zeros and remove them
-	// for i, part := range parts {
-	// 	for j, char := range part {
-	// 		if char != '0' {
-	// 			parts[i] = part[j:]
-	// 			break
-	// 		}
-	// 	}
-	// }
+	// Check each part for leading zeros and remove them
+	for i, part := range parts {
+		for j, char := range part {
+			if char != '0' {
+				parts[i] = part[j:]
+				break
+			}
+		}
+	}
 
-	// // Make sure we have exactly 3 parts separated by a dot, if we don't we append 0 to the version, if more we skip the rest
-	// if len(parts) < 3 {
-	// 	for i := len(parts); i < 3; i++ {
-	// 		version += ".0"
-	// 	}
-	// }
-	// if len(parts) > 3 {
-	// 	version = strings.Join(parts[:3], ".")
-	// }
+	// Make sure we have exactly 3 parts separated by a dot, if we don't we append 0 to the version, if more we skip the rest
+	if len(parts) < 3 {
+		for i := len(parts); i < 3; i++ {
+			parts = append(parts, "0")
+		}
+	} else if len(parts) > 3 {
+		parts = parts[:3]
+	}
+
+	version = strings.Join(parts, ".")
 
 	return version
 }
